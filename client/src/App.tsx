506c62952ca6a299162b4ab5eb93802ae4bc5773
Bekzat Gazezov <beka.g0226@gmail.com>
2025-07-27 19:12:30 +0500

    feat: added chat page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Onboarding from "./page/Onboarding";
+import ChatPage from "./page/Chat";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Onboarding />} />
+      <Route path="/chat" element={<ChatPage />} />
     </Routes>
   );
 };
