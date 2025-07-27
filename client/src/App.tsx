5d22b01d09d7fcb3ddc3ec7d4098f294421cc9eb
Bekzat Gazezov <beka.g0226@gmail.com>
2025-07-27 19:08:54 +0500

    feat: added onboarding page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
-import { Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Onboarding from "./page/Onboarding";
 
 const AppRoutes = () => {
-  return <Routes></Routes>;
+  return (
+    <Routes>
+      <Route path="/" element={<Onboarding />} />
+    </Routes>
+  );
 };
 
 export default AppRoutes;
